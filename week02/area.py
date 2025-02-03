0d959e0971ec56ab7919679e3f1f3623159fd998
kanchaporn-tnk <67319010059@tnk.ac.th>
2025-02-03 23:34:56 +0700

    Create area.py

---
+++ week02/area.py
@@ -0,0 +1,16 @@
+
+print("คำนวณหาพื้นที่ 4 เหลี่ยมจตุรัส")
+square = int(input("ใส่ความยาวด้าน: "))
+square1 = square*square
+print(f"พื้นที่ 4 เหลี่ยมจตุรัสนี้ คือ {square1}\n\n")
+
+print("คำนวณหาพื้นที่ 4 เหลี่ยมผืนผ้า")
+weight = int(input("ใส่ความกว้าง: "))
+height = int(input("ใส่ความยาว: "))
+square2 = weight*height
+print(f"พื้นที่ 4 เหลี่ยมผืนผ้านี้ คือ {square2}\n\n")
+
+print("คำนวณหาพื้นที่วงกลม")
+r = float(input("ใส่ความยาวของรัศมี: "))
+circle = 3.14*(r**2)
+print(f"พื้นที่ 4 เหลี่ยมวงกลมนี้ คือ {circle}\n")
